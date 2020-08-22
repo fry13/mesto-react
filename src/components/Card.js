4fa1081f5db32c9a203afe14fadef4bc700690a4
fry13 <ninetailed1995@gmail.com>
2020-08-23 00:13:34 +0400

    destructuring props in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function Card(props) {
+function Card({card, onCardClick}) {
   function handleClick() {
-    props.onCardClick(props.card);
+    onCardClick(card);
   }
 
   return (
     <div className="elements__card">
-      <img src={props.card.link} alt={props.card.name} className="elements__photo" onClick={handleClick} />
+      <img src={card.link} alt={card.name} className="elements__photo" onClick={handleClick} />
       <div className="elements__container">
-        <p className="elements__title">{props.card.name}</p>
+        <p className="elements__title">{card.name}</p>
         <div className="elements__like-container">
           <button type="button" className="elements__like"/>
-          <p className="elements__counter">{props.card.likes.length}</p>
+          <p className="elements__counter">{card.likes.length}</p>
         </div>
       </div>
       <button type="button" className="elements__trash-bin" />
